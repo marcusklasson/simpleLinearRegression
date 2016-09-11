fecc340b68cc096be22766944cda9fca996890ba
mklax <marcusklasson@hotmail.com>
2016-09-11 16:29:00 +0200

    Update simpleLinearRegression.py

--- simpleLinearRegression.py
+++ simpleLinearRegression.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Sep 08 18:55:05 2016
-
-@author: Marcus
 """
+
 import numpy as np
 import matplotlib.pyplot as plt
-import pylab
 
 def hypothesisFunction(x, theta0, theta1):
     h = []
@@ -17,7 +15,6 @@ def hypothesisFunction(x, theta0, theta1):
 def costFunction(x,y,theta0,theta1) :
     h = hypothesisFunction(x,theta0, theta1)
     tmp = np.subtract(h,y)
-    #print str(np.power(tmp,2)) + 'sataattana'
     J = 1/float(2*len(x)) * np.sum(np.power(tmp,2))
     return J
     
@@ -30,11 +27,8 @@ def gradientDescent(x,y,theta0,theta1,alpha,tol):
         theta0_old = theta0
         theta1_old = theta1
         theta0 = updateTheta0(x,y,theta0_old,theta1_old,alpha)
-        #print theta0
         theta1 = updateTheta1(x,y,theta0_old,theta1_old,alpha)
-        #print theta1
         J = costFunction(x,y,theta0, theta1)
-        #print J
         iter = iter + 1
     print 'Number of iterations: ' + str(iter)
     return [theta0, theta1]
@@ -53,22 +47,19 @@ def updateTheta1(x,y,theta0,theta1,alpha) :
 def main() :
     x = [0, 1, 2, 4]
     y = [1, 2, 3, 5]
-    #x = np.random.randint(100, size=100)
-    #print x
-    #y = np.random.randint(50, size=100)
-    #print y
-    theta0 = 1
-    theta1 = 1
+    theta0 = 0
+    theta1 = 0
     tol = 0.001
-    alpha = 0.2 # Learning rate
+    alpha = 0.1 # Learning rate
     Theta = gradientDescent(x,y,theta0,theta1,alpha,tol)
-    print Theta
+    print 'Theta0, Theta1 = ' + str(Theta)
     
     h = hypothesisFunction(x, Theta[0], Theta[1])
-    pylab.xlim([0,5])
-    #pylab.ylim([0,50])
-    plt.plot(x,y,'rx')
-    plt.plot(x,h,'-')
+    fig, ax = plt.subplots()
+    ax.plot(x,y,'rx')
+    ax.plot(x,h,'-')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
     plt.show() 
     
 main()
